89a47f138094c4265d5904d4a6d2136a1ccc5a06
Sherif Attia <33965805+sua-dev@users.noreply.github.com>
2023-03-01 12:14:07 +0000

    Driver class for TMP117

---
+++ src/Drivers/TMP117.py
@@ -0,0 +1,13 @@
+import board
+import busio
+import adafruit_tmp117
+
+class TMP117(adafruit_tmp117.TMP117):
+    def get_temperature(self):
+        try:
+            print(self.temperature)
+        except BaseException:
+            print("Temperature sensor not detected.\n Please check connections.")
+        
+I2C: busio.I2C = board.STEMMA_I2C()
+'''I2C bus (for TMP module)'''
