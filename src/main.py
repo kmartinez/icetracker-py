55cbd5f5bf939502fba0738d771adca8fceb6799
Sherif Attia <33965805+sua-dev@users.noreply.github.com>
2023-05-30 17:19:21 +0100

    appended time_window code in main.py. Commented out alarm_is_in_future as that forces an abnormal reset for both devices. Base and Rover can now possibly wake up at same time windows together.

--- src/main.py
+++ src/main.py
@@ -1,39 +1,102 @@
-"""This module is executed by circuitpython upon startup.
-It first checks if the module was reset abnormally, and then if so shuts down gracefully.
-Otherwise, the module determines if it is configured as a base or a rover, where it will then execute the respective module
-"""
-
 from config import *
 import os
 import Drivers.PSU as PSU
-from Drivers.RTC import RTC_DEVICE
+from Drivers.I2C_Devices import RTC_DEVICE
 from Drivers.SPI_SD import *
 import traceback
 from microcontroller import watchdog
 from watchdog import WatchDogMode
 import adafruit_logging as logging
-from time import localtime, mktime
+from time import localtime, mktime, struct_time
+# from spi_sd import *
 import rtc
 import supervisor
 
 logger = logging.getLogger("MAIN_FILE")
 rtc.set_time_source(RTC_DEVICE)
 
+TIME_INTERVAL = 20
+WAKEUP_INTERVAL = 10
+
+
+WAKE_UP_WINDOW_HRS  = [0, 3, 6, 9, 12, 15, 18, 21] 
+# WAKE_UP_WINDOW_MINS = [0,5,10,15,20,25,30,35,40,45,50,55]
+WAKE_UP_WINDOW_MINS = [0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57]
+
+def get_next_alarm_hour(hh):
+    nexttime = None
+
+    if ( hh in WAKE_UP_WINDOW_HRS):
+            position = WAKE_UP_WINDOW_HRS.index(hh)
+            next_position = position + 1
+            if (next_position > (len(WAKE_UP_WINDOW_HRS)-1)):
+                next_position = 0
+            nexttime = WAKE_UP_WINDOW_HRS[next_position]
+    else:
+        for i in WAKE_UP_WINDOW_HRS:
+            if( i > hh):
+                nexttime = i
+                break
+        if nexttime == None:
+            nexttime = 0
+    return nexttime
+
+def get_next_alarm_min(mm):
+    nexttime = None
+
+    if ( mm in WAKE_UP_WINDOW_MINS):
+            position = WAKE_UP_WINDOW_MINS.index(mm)
+            next_position = position + 1
+            if (next_position > (len(WAKE_UP_WINDOW_MINS)-1)):
+                next_position = 0
+            nexttime = WAKE_UP_WINDOW_MINS[next_position]
+    else:
+        for i in WAKE_UP_WINDOW_MINS:
+            if( i > mm):
+                nexttime = i
+                break
+        if nexttime == None:
+            nexttime = 0
+    return nexttime
+
+
 if __name__ == "__main__":
       if ADMIN_IO.value:
             supervisor.set_next_code_file("./adminmode.py")
 
+      logger.info("Current Time")
+      print(RTC_DEVICE.datetime)
+      (YY, MM, DD, hh, mm, ss, wday, yday, dst) = RTC_DEVICE.datetime
+      nextwake_MM = get_next_alarm_min(mm)
+      logger.info("NEXT WAKEUP TIME (Minutes) {}".format(nextwake_MM))
+      RTC_DEVICE.alarm1 = (struct_time([YY,MM,DD,hh,nextwake_MM,0,wday,yday,dst]), "monthly")
+      print(RTC_DEVICE.alarm1)
+
+      # print("Setting First Alarm")
+      # first_alarm_time = localtime(mktime(RTC_DEVICE.datetime)+TIME_INTERVAL)
+
+      # RTC_DEVICE.alarm1 = (first_alarm_time, "monthly")
+      print(RTC_DEVICE.alarm1_status)
       if RTC_DEVICE.alarm1_status:
-         if RTC_DEVICE.alarm_is_in_future():
-            logger.critical("Abnormal reset detected!!! Shutting device down")
-            PSU.shutdown()
-         else:
-            RTC_DEVICE.alarm1 = (localtime(mktime(RTC_DEVICE.alarm1[0])+300), "monthly")
-      RTC_DEVICE.alarm1_interrupt = True
+         """ALARM in future forces the device to reset immediatley upon waking up, need solution around this..."""
+         """Devices can now wake up at certain time windows throughout the day"""
+         # if RTC_DEVICE.alarm_is_in_future():
+         #    logger.critical("Abnormal reset detected!!! Shutting device down")
+         #    PSU.shutdown()
+         # else:
+            # RTC_DEVICE.alarm1 = (localtime(mktime(RTC_DEVICE.alarm1[0])+TIME_INTERVAL), "monthly")
+            # RTC_DEVICE.alarm1 = (localtime(mktime(RTC_DEVICE.datetime)+30), "monthly")
+         (YY, MM, DD, hh, mm, ss, wday, yday, dst) = RTC_DEVICE.datetime
+         nextwake_MM = get_next_alarm_min(mm)
+         RTC_DEVICE.alarm1 = (struct_time([YY,MM,DD,hh,nextwake_MM,0,wday,yday,dst]), "monthly")
+         print(RTC_DEVICE.alarm1)
+
+      # RTC_DEVICE.alarm1_interrupt = True
 
       if not DEBUG["WATCHDOG_DISABLE"]:
          watchdog.timeout = 16
          watchdog.mode = WatchDogMode.RESET
+        #  PSU.shutdown()
          watchdog.feed()
       try:
          if "data_entries" not in os.listdir("/sd/"):
