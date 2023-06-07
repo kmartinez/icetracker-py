b8e1a50107f3d78b5646e5c031feb229dd23b170
Sherif Attia <33965805+sua-dev@users.noreply.github.com>
2023-06-07 09:30:25 +0100

    added offset calibration, which is only populated once, providing atan(result) to one slope

--- src/config.py
+++ src/config.py
@@ -1,53 +1,63 @@
-"""Configuration file for the device.
-Make sure WATCHDOG_DISABLE = False for deployments!
-"""
-
 import adafruit_logging as logging
 import board
-from busio import UART, SPI
+from busio import UART, SPI, I2C
 from digitalio import DigitalInOut
+from Drivers.I2C_Devices import *
 
-
-
+"""PIN Definitions"""
+# I2C = board.I2C()
+'''Main I2C Bus for communication with TMP117, GPS NMEA and DS3231 Chip'''
 ADMIN_IO = DigitalInOut(board.A2)
 '''Main Admin IO Pin'''
-COMMS_EN = DigitalInOut(board.D12)
-COMMS_EN.switch_to_output(value=False)
-'''ENABLE LR COMMS'''
 GPS_EN = DigitalInOut(board.D4)
 GPS_EN.switch_to_output(value=False)
 '''ENALBE GPS'''
 BATV_EN = DigitalInOut(board.A1)
 BATV_EN.switch_to_output(value=False)
 '''ENABLE BATV INPUT'''
-"""Pin Definitions"""
+
+def calibrate_offset():
+    """Calibrates offset by populating once
+
+    Returns:
+        float: atan of calibrated offset to one slope
+    """
+    ADXL_343.offset = (0,0,0)
+    x = ADXL_343.raw_x
+    y = ADXL_343.raw_y
+    z = ADXL_343.raw_z
+
+    ADXL_343.offset(
+        round(-x / 8),
+        round(-y / 8),
+        round(-(z - 250) / 8), 
+    )
+    x, y ,z = ADXL_343.offset
+    slope = ADXL_343.accelerometer_to_slope(x,y)
+    return slope
 
 
-GLOBAL_FAILSAFE_TIMEOUT = 600
+
+GLOBAL_FAILSAFE_TIMEOUT = 120
 '''If this amount of seconds passes program should abort and base should send whatever it has'''
 TIME_BETWEEN_WAKEUP = 180
 '''Amount of seconds RTC should wait before setting off the alarm again (waking up the system in the process)'''
-DEVICE_ID = 0
-'''ID of this device (Device will become a base station if this is 0)'''
+DEVICE_ID = 18
+'''ID of this device (Will become base station if is 0)'''
 ROVER_COUNT = 1
-'''Number of rovers total (not including base stations)'''
-
-
 
 DEBUG =  {
     "FAKE_DATA": False, #Ignores actual GPS data and just uses fake static data instead
     "WATCHDOG_DISABLE": True #Disables watchdog resets so you can debug things
 }
-'''Debugging configuration. See actual module file for details'''
 
-#Set log levels of individual loggers here
 logging.getLogger("BASE").setLevel(logging.INFO)
 logging.getLogger("DEVICE").setLevel(logging.INFO)
 logging.getLogger("ROVER").setLevel(logging.INFO)
 logging.getLogger("ASYNC_UART").setLevel(logging.INFO)
 logging.getLogger("RADIO").setLevel(logging.INFO)
 logging.getLogger("MAIN_FILE").setLevel(logging.INFO)
-logging.getLogger("GPS").setLevel(logging.INFO)
+logging.getLogger("GPS").setLevel(logging.DEBUG)
 
 
 SECRETS = { #I would put this on its own file untracked by git if you ever put any actual sensitive info here
@@ -55,4 +65,3 @@ SECRETS = { #I would put this on its own file untracked by git if you ever put a
     "apn_username": "",
     "apn_password": "",
 }
-
