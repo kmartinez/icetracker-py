d0bb63d494b09ed91de88be1cd56d3e50f80e43c
Sherif Attia <33965805+sua-dev@users.noreply.github.com>
2023-06-20 17:06:42 +0100

    Update config.py - UPDATED by removing extra comments and supporting global ACC variables.

--- src/config.py
+++ src/config.py
@@ -4,27 +4,28 @@ from busio import UART, SPI, I2C
 from digitalio import DigitalInOut
 
 DEVICE_ID = 18
-'''ID of this device'''
+'''ID of this device (Will become base station if is 100 or higher)'''
 # accel offsets (find manually and enter here)
 ACC_X_OFF = 0
 ACC_Y_OFF = 0
 ANT_OFFSET = 0
-# SET FOR BASE STATION ONLY
+#SET FOR BASE STATION ONLY
 ROVER_COUNT = 1
 
 """PIN Definitions"""
-# I2C = board.I2C()
-'''Main I2C Bus for communication with TMP117, GPS NMEA and DS3231 Chip'''
 ADMIN_IO = DigitalInOut(board.A2)
 '''Main Admin IO Pin'''
+COMMS_EN = DigitalInOut(board.D12)
+COMMS_EN.switch_to_output(value=False)
+'''ENABLE LR COMMS - BASE ONLY'''
 GPS_EN = DigitalInOut(board.D4)
 GPS_EN.switch_to_output(value=False)
-'''ENABLE GPS'''
+'''ENALBE GPS'''
 BATV_EN = DigitalInOut(board.A1)
 BATV_EN.switch_to_output(value=False)
 '''ENABLE BATV INPUT'''
 
-GLOBAL_FAILSAFE_TIMEOUT = 120
+GLOBAL_FAILSAFE_TIMEOUT = 180
 '''If this amount of seconds passes program should abort and base should send whatever it has'''
 
 DEBUG =  {
