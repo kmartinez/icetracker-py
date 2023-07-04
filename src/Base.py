5fbd84fceaf87dd0f4c09570d76366cee568eb3e
Michael Jones <mwj2g19@soton.ac.uk>
2023-07-04 12:19:44 +0100

    fix bug causing rtcm task to crash (only matters for >1 rover)

--- src/Base.py
+++ src/Base.py
@@ -75,9 +75,10 @@ async def rtcm3_loop():
         if len(rtcm3_rovers) < 1:
             rtcm3_pause = False
         else:
-            for k,v in rtcm3_rovers:
+            for k,v in rtcm3_rovers.items():
                 if v:
                     rtcm3_pause = True
+                    break
             rtcm3_pause = False
         
         logger.debug(f"RTCM3_PAUSE: {rtcm3_pause}")
@@ -92,6 +93,7 @@ async def rover_data_loop():
             logger.info("Waiting for a radio packet")
             packet = await radio.receive_packet()
             logger.info(f"packet received from {packet.sender}")
+            logger.debug(f"PACKET TYPE: {packet.type}")
         except radio.ChecksumError:
             logger.warning("Radio received invalid packet")
             continue
@@ -128,6 +130,7 @@ async def rover_data_loop():
             radio.send_response(PacketType.ACK, packet.sender)
 
         elif packet.type == PacketType.FIN and struct.unpack(radio.FormatStrings.PACKET_DEVICE_ID, packet.payload)[0] == DEVICE_ID:
+            logger.info(f"FIN RECEIVED FROM {packet.sender}")
             rtcm3_rovers[packet.sender] = False
             finished_rovers[packet.sender] = True
             await asyncio.sleep(3)
