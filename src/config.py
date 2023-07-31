3d841f8b84dbb2a50d331e91aa66c02651a3ab00
Kirk Martinez <km@ecs.soton.ac.uk>
2023-07-31 12:26:51 +0100

    comments

--- src/config.py
+++ src/config.py
@@ -6,8 +6,8 @@ from digitalio import DigitalInOut
 DEVICE_ID = 18
 '''ID of this device (Will become base station if is 100 or higher)'''
 # accel offsets (find manually and enter here)
-ACC_X_OFF = 0
-ACC_Y_OFF = 0
+#ACC_X_OFF = 0
+#ACC_Y_OFF = 0
 ANT_OFFSET = 0
 #SET FOR BASE STATION ONLY
 ROVER_COUNT = 1
@@ -22,8 +22,9 @@ GPS_EN = DigitalInOut(board.D4)
 GPS_EN.switch_to_output(value=False)
 '''ENABLE GPS'''
 BATV_EN = DigitalInOut(board.A1)
-BATV_EN.switch_to_output(value=False)
 '''ENABLE BATV INPUT'''
+BATV_EN.switch_to_output(value=False)
+
 
 GLOBAL_FAILSAFE_TIMEOUT = 180
 '''If this amount of seconds passes program should abort and base should send whatever it has'''
@@ -37,11 +38,13 @@ LOGGER_NAMES = ["BASE", "DEVICE", "ROVER", "ASYNC_UART", "RADIO", "MAIN_FILE", "
 for name in LOGGER_NAMES:
     logging.getLogger(name).setLevel(logging.INFO)
 
-COMMS_TIME = [13] #Final version should be 13 or 14
+# Rules: wake each 3hrs including the coms time hour. Breida 100 uses min=0 fjalls base 101 uses 30
+COMMS_TIME = [13] #Final version should be 13
 WAKE_UP_WINDOW_HRS  = [0, 3, 6, 9, 12, 13, 15, 18, 21]
+# fjalls MINS=30 Breida 0
 WAKE_UP_WINDOW_MINS = [0]
 
-BASE_ID = 100 #Rover now needs to know this
+BASE_ID = 100 #Rover now needs to know this.
 
 MAX_READINGS_IN_SINGLE_HTTP = 4
 
