013799c244f6f902965966fdd1dae7b0452061e9
Sherif Attia <33965805+sua-dev@users.noreply.github.com>
2023-09-18 16:35:32 +0100

    Update manual_post.py - tidying up, removing extra comments and libs.

--- src/manual_post.py
+++ src/manual_post.py
@@ -1,19 +1,11 @@
 # Manual AT HTTP post
 
 from adafruit_fona.adafruit_fona import FONA
-import adafruit_fona.adafruit_fona_network as network
-import adafruit_fona.adafruit_fona_socket as cellular_socket
 from Drivers.SPI_SD import *
 import time
 from Drivers.PSU import * #EVERYTHING FROM THIS IS READONLY (you can use write functions, but cannot actually modify a variable)
 from config import *
 import json
-import adafruit_requests as requests
-import asyncio
-import struct
-import os
-from microcontroller import watchdog
-from watchdog import WatchDogMode
 import adafruit_logging as logging
 
 logger = logging.getLogger("BASE")
@@ -33,39 +25,25 @@ http_payload = '{"sats": 12, "temp": 2.35, "altitude": 105.637, "timestamp": "20
 def http_post(payload) -> bool:
 
     logger.info("SETTING UP URL")
-    # print(fona._send_check_reply(b"AT+HTTPPARA=\"URL\",\"http://marc.ecs.soton.ac.uk/postin\"",reply=REPLY_OK))
     if not fona._send_check_reply(b"AT+HTTPPARA=\"URL\",\"http://marc.ecs.soton.ac.uk/postin\"",reply=REPLY_OK):
         return False       # set HTTP parameters value
     time.sleep(0.1)
 
     logger.info("PROVIDING CONTENT TYPE")
-    # print(fona._send_check_reply(b"AT+HTTPPARA=\"CONTENT\",\"text/plain\"",reply=REPLY_OK))
-    # if not fona._send_check_reply(b"AT+HTTPPARA=\"CONTENT\",\"text/plain\"",reply=REPLY_OK):
-    if not fona._send_check_reply(b"AT+HTTPPARA=\"CONTENT\",\"application/json\"",reply=REPLY_OK):
     # if not fona._send_check_reply(b"AT+HTTPPARA=\"CONTENT\",\"x-www-form-urlencoded\"",reply=REPLY_OK):
+    # if not fona._send_check_reply(b"AT+HTTPPARA=\"CONTENT\",\"text/plain\"",reply=REPLY_OK):
+
+    if not fona._send_check_reply(b"AT+HTTPPARA=\"CONTENT\",\"application/json\"",reply=REPLY_OK):
         return False        # set HTTP parameters value
     time.sleep(0.1)
 
 
     logger.info("PROVIDING PAYLOAD")
-
-    # # print(str(payload))
-    # print(str(len(payload)))
-    # print(fona._uart_write(b"AT+HTTPDATA=str(len(payload)),5000"))
-    # print( fona._send_check_reply(b"AT+HTTPDATA=str(len(payload)),5000",reply=REPLY_OK))
     if not fona._send_check_reply(b"AT+HTTPDATA="+str(len(payload))+",10000",reply=REPLY_DN): # - Fails here unsupported types
-    # while True:
-    #     # print(fona._send_check_reply(b"AT+HTTPDATA=str(len(payload)),100000",reply=REPLY_OK))
-    #     print(fona._send_check_reply(b"AT+HTTPDATA=192,5000",reply=REPLY_OK))
-    #     time.sleep(0.1)
         return False      # input HTTP data
     else:
-        # fona._uart_write(bytearray(payload[0]))
         fona._uart_write(str(json.loads(payload)).encode())
-
     time.sleep(0.1)
-
-    
 
     logger.info("POSTING DATA")
     if not fona._send_check_reply(b"AT+HTTPACTION=1",reply=REPLY_OK):
@@ -85,14 +63,6 @@ def http_post(payload) -> bool:
 
     return True
 
-
-# def main():
-    # print(len(http_payload))
-    # print(str(json.loads(http_payload)).encode())
-    # print((json.loads(http_payload)))
-
-# if __name__ == '__main__':
-#     main()
     
 if __name__ == '__main__':
     
@@ -102,17 +72,10 @@ if __name__ == '__main__':
     
     logger.info("FONA initialized")
 
-    # fona._send_ckeck_reply(b"ATE0",reply=fona.REPLY_OK)
-    # print(fona.network_status)
-    # print(fona._uart_write(CMD_AT))
+
     fona._send_check_reply(CMD_AT,reply=REPLY_OK) # True # sends data to fona - validates if true - send AT - expecting response OK - received True 
-    # time.sleep(0.1)
-    # print(fona._read_line()) #(0, b'')
-    # print(fona._uart_write(b"ATE0"))
-    # print(fona._read_line())
+
     (fona._send_check_reply(b"ATE0",reply=REPLY_OK)) # using send_check_reply alone responds with OK which means echoing is now turned off.
-    # time.sleep(0.1)
-    # print(fona._read_line()) # reads multiple lines into the buffer - optionally prints the buffer after reading
 
     # enable gprs - using apn
     while not fona.gprs:
@@ -120,14 +83,11 @@ if __name__ == '__main__':
     print(fona.gprs) # retruns false - need to enable using set_gprs() which needs apn, username and pw
     logger.info("GPRS ENABLED")
 
-    # AT+HTTPCPOST
-    # print(fona.local_ip)
+    # AT+HTTPCPOST - https://docs.espressif.com/projects/esp-at/en/latest/esp32/AT_Commands_Set/HTTP_AT_Commands.html#cmd-httpcpost
 
     (fona._send_check_reply(b"AT+HTTPINIT",reply=REPLY_OK)) # Initialise HTTP service return True
     time.sleep(0.1)
     (fona._send_check_reply(b"AT+HTTPPARA=\"CID\",1",reply=REPLY_OK)) # HTTPPARA paramter - sets the bearer profile ID of the connection - returns true
     logger.info("HTTP SERVICES ENABLED")
 
-
-    # print(fona._send_check_reply(b"AT+HTTPPARA=\"URL\",\"http://marc.ecs.soton.ac.uk/postin\"",reply=REPLY_OK)) # we want to provide a URL instead of CID
     (http_post(http_payload))
